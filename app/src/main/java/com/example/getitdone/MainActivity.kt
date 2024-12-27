e4cdfc7f44a4f4a1a93890fec6b4210f4b5dc90a
Atishay <v.kandarp@gmail.com>
2024-12-27 15:41:16 +0530

    Added FAB onclick listener.

--- app/src/main/java/com/example/getitdone/MainActivity.kt
+++ app/src/main/java/com/example/getitdone/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.getitdone
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.getitdone.databinding.ActivityMainBinding
+import com.example.getitdone.databinding.DialogAddTaskBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +23,26 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(binding.tabs, binding.pager) { tab, position ->
             tab.text = "Tasks"
         }.attach()
+
+        binding.fab.setOnClickListener {
+            showAddTaskDialog()
+        }
+    }
+
+    private fun showAddTaskDialog() {
+        val dialogBinding = DialogAddTaskBinding.inflate(layoutInflater)
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Add Task")
+            .setView(null)
+            .setPositiveButton("Save") { _, _ ->
+                Toast.makeText(
+                    this,
+                    "Task is ${dialogBinding.editTextTask.text}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 
     inner class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
