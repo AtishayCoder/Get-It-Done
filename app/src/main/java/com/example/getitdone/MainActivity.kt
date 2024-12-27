954fa6dfa6da6fedf21e14b256eececc9ba1e623
Atishay <v.kandarp@gmail.com>
2024-12-27 15:06:11 +0530

    Setup Fragment and created ViewPager adapter class.

--- app/src/main/java/com/example/getitdone/MainActivity.kt
+++ app/src/main/java/com/example/getitdone/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.getitdone
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.getitdone.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding.pager.adapter = PagerAdapter(this)
+        TabLayoutMediator(binding.tabs, binding.pager) { tab, position ->
+            tab.text = "Tasks"
+        }.attach()
+    }
+
+    inner class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+        override fun getItemCount(): Int = 1
+
+        override fun createFragment(position: Int): Fragment {
+            return TasksFragment()
         }
+
     }
 }
