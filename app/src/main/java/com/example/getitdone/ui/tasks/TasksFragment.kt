e783bab94bdd6de4a523d802cb9338baa77957ef
Atishay <v.kandarp@gmail.com>
2025-02-25 09:41:22 +0530

    Used ViewModel and Application class

--- app/src/main/java/com/example/getitdone/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/getitdone/ui/tasks/TasksFragment.kt
@@ -6,11 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.getitdone.data.GetItDoneDatabase
 import com.example.getitdone.data.Task
-import com.example.getitdone.data.TaskDao
 import com.example.getitdone.databinding.TasksFragmentBinding
-import kotlin.concurrent.thread
 import kotlin.getValue
 
 class TasksFragment : Fragment(), TasksAdapter.TaskClickListener {
